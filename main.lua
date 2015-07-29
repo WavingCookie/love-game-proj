5e888ab6b24aa08e4942e230078a0d11c1164de1
WavingCookie <wavingrainbow@live.com>
2015-07-29 21:34:25 +0200

    Gravity !

--- main.lua
+++ main.lua
@@ -6,6 +6,7 @@ function love.load()
 
 	setScreen = love.window.setMode(640,480, {vsync=true, fsaa=4})
 	plyr_sprite = love.graphics.newImage("gfx/player.png")
+	bg = love.graphics.newImage("gfx/clouds.png")
 	player_x = 64
 	player_y = 64
 	player_vs = 0
@@ -29,35 +30,35 @@ function love.update(dt)
 			local dFps = love.timer.getFPS()
 
 			love.graphics.print("FPS:" .. tostring(dFps),32,32)
+			love.graphics.setBackgroundColor(0,191,255)
+			love.graphics.draw(bg,0,0)
 			love.graphics.draw(plyr_sprite,player_x,player_y)
 
 		end
 
-		function love.keypressed(key)
+		player_y = player_y + player_vs
+		if player_y > 360 then
 
-			if key == 'a' or key == 'q' then
+			player_y = 360
+			player_vs = 0
 
-				player_x = player_x - 16
+		else
+
+			if player_vs == 0 then
+
+				player_vs = 1
+
+			elseif player_vs > 0 and player_vs < 8 then
+
+				player_vs = player_vs * (1.05)
+
+			else
+
+				player_vs = 8
 
 			end
 
 		end
-
-		if player_vs == 0 then
-
-			player_vs = 1
-
-		elseif player_vs > 0 and player_vs < 8 then
-
-			player_vs = player_vs * (1.05)
-
-		else
-
-			player_vs = 8
-
-		end
-
-		player_y = player_y + player_vs
 
 		uC = uC - uM
 
