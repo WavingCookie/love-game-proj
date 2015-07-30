ef3b8ab8d22310c7f2227f0e138c95aa2bf223ca
WavingCookie <wavingrainbow@live.com>
2015-07-30 17:08:44 +0200

    A Small amount of things

--- main.lua
+++ main.lua
@@ -1,11 +1,20 @@
 local fps = 60
+local animCounter = 0
+local animPlayer = 0
+local facingDirection = "right"
+local jump = love.keyboard.isDown("z","w") -- AZERTY and QWERTY support
+local left = love.keyboard.isDown("q","a") -- Same here
+local right = love.keyboard.isDown("d") -- Nothing to add here
+
 uM = 1 / fps
 uC = 0
 
 function love.load()
 
 	setScreen = love.window.setMode(640,480, {vsync=true, fsaa=4})
-	plyr_sprite = love.graphics.newImage("gfx/player.png")
+	plyr_r_1 = love.graphics.newImage("gfx/player/1_r.png")
+	plyr_id = love.graphics.newImage("gfx/player/2_r.png")
+	plyr_r_3 = love.graphics.newImage("gfx/player/3_r.png")
 	bg = love.graphics.newImage("gfx/clouds.png")
 	player_x = 64
 	player_y = 64
@@ -25,14 +34,25 @@ function love.update(dt)
 
 	while uC >= uM do
 
+		if right then
+
+			
+
+		end
+
 		function love.draw()
 
 			local dFps = love.timer.getFPS()
+            if dFps > 60 then
+            
+                dFps = 60
+            
+            end
 
 			love.graphics.print("FPS:" .. tostring(dFps),32,32)
 			love.graphics.setBackgroundColor(0,191,255)
 			love.graphics.draw(bg,0,0)
-			love.graphics.draw(plyr_sprite,player_x,player_y)
+			love.graphics.draw(plyr_id,player_x,player_y)
 
 		end
 
